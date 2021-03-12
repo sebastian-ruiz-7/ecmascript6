c149ebac12cd83a30460f103ac5dc186cbaf783d
Sebastian Ruiz <sebastianruizalvarez@gmail.com>
2021-03-11 19:51:14 -0600

    Se estudió el uso del método includes

---
+++ src/es7/index.js
@@ -0,0 +1,32 @@
+//Here is goint to study what was added in ES7
+
+//Includes method. This works with arrays and strings.
+//This allows us knows if inside a value are an element 
+
+//For example, we use the includes method to see if inside this array are the number 7
+let numbers=[1,2,3,7,8];
+
+ if(numbers.includes(7)){
+     console.log('Sí se encuentra el valor');
+ } else {
+    console.log('No se encuentra.')
+ }
+
+ //Output: Sí se encuentra
+
+ if(numbers.includes(4)){
+    console.log('Sí se encuentra el valor');
+} else {
+   console.log('No se encuentra.')
+}
+
+ //Output: No se encuentra
+
+
+ let str='Ayer pasé por tu casa cate de mi corazón. Soy aguacate';
+
+ if(str.includes('ayer')){
+     console.log('Adivinaste')
+ }else{
+    console.log('Te la persignaste krnal :(');
+ }
